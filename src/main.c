43786257962e7210b03fbdb6a44b1401ee4fc5b1
PM <phillip.meisinger@gmx.de>
2024-11-29 20:12:59 +0100

    removed map function

--- src/main.c
+++ src/main.c
@@ -61,13 +61,6 @@ int main(void)
     }
 }
 
-long map(long x, long in_min, long in_max, long out_min, long out_max)
-
-    {
-
-      return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-
-    }
 
 //--------------------------------------------------------------------+
 // Device callbacks
