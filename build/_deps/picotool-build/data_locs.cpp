d9a2b11aaeb45a6ab4ba1f13b94247abeadb38ed
PM <phillip.meisinger@gmx.de>
2025-02-09 09:37:28 +0100

    gitignore build

--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","C:/RP2040/dev/midi_test/build/_deps/picotool/"
-};
--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@
 
 #define BUTTON_PIN 20
 #define LED_PIN 22
-#define ADC_PIN 26
+#define ADC_PIN 27
 #define PWM_PIN 11
 
 #define ROT_SW_CLK_PIN 2
@@ -36,7 +36,7 @@ void led_setup(void);
 void adc_setup(void);
 void rot_sw_setup(void);
 
-void led_blinking_task(void);
+void button_task(void);
 void adc_task(void);
 void midi_task(void);
 void rot_sw_task(void);
@@ -55,7 +55,7 @@ int main(void)
     while (1)
     {
         tud_task(); // tinyusb device task
-        led_blinking_task();
+        button_task();
         adc_task();
         rot_sw_task();
     }
@@ -92,50 +92,6 @@ void tud_resume_cb(void)
 #pragma endregion
 
 //--------------------------------------------------------------------+
-// MIDI Task
-//--------------------------------------------------------------------+
-
-/*void midi_task(void)
-{
-    return;
-    static uint32_t start_ms = 0;
-
-    uint8_t const cable_num = 0; // MIDI jack associated with USB endpoint
-    uint8_t const channel   = 0; // 0 for channel 1
-
-    // The MIDI interface always creates input and output port/jack descriptors
-    // regardless of these being used or not. Therefore incoming traffic should be read
-    // (possibly just discarded) to avoid the sender blocking in IO
-    uint8_t packet[4];
-    while ( tud_midi_available() ) tud_midi_packet_read(packet);
-
-    // send note periodically
-    if (board_millis() - start_ms < 286) return; // not enough time
-    start_ms += 286;
-
-    // Previous positions in the note sequence.
-    int previous = (int) (note_pos - 1);
-
-    // If we currently are at position 0, set the
-    // previous position to the last note in the sequence.
-    if (previous < 0) previous = sizeof(note_sequence) - 1;
-
-    // Send Note On for current position at full velocity (127) on channel 1.
-    uint8_t note_on[3] = { 0x90 | channel, note_sequence[note_pos], 127 };
-    tud_midi_stream_write(cable_num, note_on, 3);
-
-    // Send Note Off for previous note.
-    uint8_t note_off[3] = { 0x80 | channel, note_sequence[previous], 0};
-    tud_midi_stream_write(cable_num, note_off, 3);
-
-    // Increment position
-    note_pos++;
-
-    // If we are at the end of the sequence, start over.
-    if (note_pos >= sizeof(note_sequence)) note_pos = 0;
-}*/
-
-//--------------------------------------------------------------------+
 // BLINKING TASK
 //--------------------------------------------------------------------+
 void led_setup(void)
@@ -150,13 +106,22 @@ void led_setup(void)
 }
 bool button_state = 0;
 bool button_state_last = 0;
-void led_blinking_task(void)
+void button_task(void)
 {
     button_state_last = button_state;
     button_state = gpio_get(BUTTON_PIN);
 
-    uint8_t packet[4];
-    while ( tud_midi_available() ) tud_midi_packet_read(packet);
+    uint8_t packet[4] = { 0, 0, 0, 0 };
+    
+    tud_midi_packet_read(packet);
+    for( int i = 0; i < 4; i++ )
+    {
+        if ( packet[i] != 0 )
+        {
+            static uint debug = 0;
+            debug++;
+        }
+    }
 
     if ( button_state && !button_state_last )
     {
@@ -174,12 +139,6 @@ void adc_setup(void)
     adc_init();
     adc_gpio_init( ADC_PIN );
 
-    //gpio_init(11);
-    //gpio_set_dir(11, true );
-    //gpio_put(11, true);
-    //sleep_ms(500);
-    //gpio_put(11, false);
-    //gpio_deinit(11);
 
     gpio_set_function( PWM_PIN, GPIO_FUNC_PWM );
     uint slice_num = pwm_gpio_to_slice_num(11);
@@ -189,14 +148,6 @@ void adc_setup(void)
     pwm_set_enabled( slice_num, true );
     
     pwm_set_gpio_level( PWM_PIN, 127 );
-    //sleep_ms(2000);
-    //pwm_set_gpio_level( 11, 127/2 );
-    //sleep_ms(2000);
-    //pwm_set_gpio_level( 11, 128 );
-    //sleep_ms(2000);
-    //pwm_set_gpio_level( 11, 128 );
-    //sleep_ms(5000);
-
     
 
     for ( uint16_t i = 0; i <= 127; i++ )
@@ -208,13 +159,14 @@ void adc_setup(void)
 
 uint16_t result_0_127 = 0;
 uint16_t result_0_127_pref = 0;
+uint16_t adc_result;
 void adc_task(void)
 {
     gpio_put( LED_PIN, 1 );
     
     gpio_put( LED_PIN, !gpio_get(LED_PIN ) );
-    adc_select_input( 0 );
-    uint16_t adc_result = adc_read();
+    adc_select_input( 1 );
+    /*uint16_t*/ adc_result = adc_read();
     
     result_0_127 = adc_result >> (12-7);
 
@@ -229,16 +181,9 @@ void adc_task(void)
     }
 }
 uint8_t sw_ctr = 0;
-uint32_t time_of_last_clk = 0;
-uint32_t time_of_last_clk_pref = 0;
 void sw_interupt_callback()
 {
     bool data = gpio_get(ROT_SW_DATA_PIN);
-
-    //time_of_last_clk_pref = time_of_last_clk;
-    //time_of_last_clk = to_ms_since_boot(get_absolute_time());
-    //
-    //if ( time_of_last_clk - time_of_last_clk_pref < 25) return;
 
     if ( data != 0 )
     {
@@ -258,7 +203,6 @@ void sw_interupt_callback()
             //tud_midi_stream_write( 0, note_on, 3);
         }
     }
-    gpio_put(7,!gpio_get(7));
 }
 void rot_sw_setup(void)
 {
