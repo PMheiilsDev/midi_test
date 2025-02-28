7153e0500fb7c8f8f4a9484c9ba3a0cf6578ce43
PM dev <PMheils.dev@gmail.com>
2025-02-28 20:17:18 +0100

    moved adc handler

--- midi_test.c
+++ midi_test.c
@@ -18,6 +18,7 @@
 
 #include "rotarty_switch.h"
 #include "midi_handler.h"
+#include "adc_handler.h"
 
 #pragma endregion
 
@@ -26,9 +27,6 @@
 #pragma region defines
 
 #define BUTTON_PIN 20
-#define LED_PIN 22
-#define ADC_PIN 27
-#define PWM_PIN 11
 
 
 #pragma endregion
@@ -144,62 +142,6 @@ void button_task(void)
         tud_midi_stream_write( 0, note_on, 3);
     }
 
-}
-
-void adc_setup(void)
-{
-    adc_init();
-    adc_gpio_init( ADC_PIN );
-
-
-    gpio_set_function( PWM_PIN, GPIO_FUNC_PWM );
-    uint slice_num = pwm_gpio_to_slice_num(11);
-
-    pwm_set_wrap(slice_num, 127);
-
-    pwm_set_enabled( slice_num, true );
-    
-    pwm_set_gpio_level( PWM_PIN, 127 );
-    
-
-    for ( uint16_t i = 0; i <= 127; i++ )
-    {
-        pwm_set_gpio_level( PWM_PIN, i );
-        sleep_ms( 10 );
-    }
-}
-
-uint16_t result_0_127 = 0;
-uint16_t result_0_127_pref = 0;
-uint16_t adc_result;
-void adc_task(void)
-{
-    gpio_put( LED_PIN, 1 );
-    
-    gpio_put( LED_PIN, !gpio_get(LED_PIN ) );
-    adc_select_input( 1 );
-    /*uint16_t*/ adc_result = adc_read();
-    
-    // change this "mapping" so that all 
-    if( adc_result <= 200 )
-    {
-        result_0_127 = 0;
-    }
-    else
-    {
-        //result_0_127 = adc_result >> (12-7);
-        result_0_127 = (((adc_result-200)*(127-80))/(4096-200))+80;
-    }
-
-    //pwm_set_gpio_level( PWM_PIN, result_0_127 );
-    //pwm_set_gpio_level( PWM_PIN, map(adc_result, 0, 65535, 0, 127 ) );
-    long difference = (long)(result_0_127_pref - result_0_127);
-    if ( difference > 1 || difference < -1 ) 
-    {
-        result_0_127_pref = result_0_127;
-        uint8_t note_on[3] = { 0b10110000 | 0, 81, result_0_127 };
-        tud_midi_stream_write( 0, note_on, 3);
-    }
 }
 
 #pragma region process midi 
---
+++ src/Inputs/Poti/adc_handler.h
@@ -0,0 +1,20 @@
+#ifndef _ADC_HANDLER_H
+#define _ADC_HANDLER_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+
+#include "hardware/pwm.h"
+#include "tusb.h"
+
+
+#define ADC_PIN 27
+#define PWM_PIN 11
+#define LED_PIN 22
+
+
+#endif
