b17a318202f213d93bb6f034e143e19c7db0c576
PM dev <PMheils.dev@gmail.com>
2025-03-02 16:24:43 +0100

    extended to more than one rot sw switch with AI

--- src/Inputs/RotarySW/rotarty_switch.c
+++ src/Inputs/RotarySW/rotarty_switch.c
@@ -1,83 +1,77 @@
+
 #include "rotarty_switch.h"
 
-
-const uint8_t rot_sw_note = 82;
-
-volatile uint8_t sw_ctr = 0;
-volatile uint8_t sw_ctr_last_sent = 0;
-
+rotary_switch_t rotary_switches[MAX_ROTARY_SWITCHES] = 
+{
+    {2, 3, 82, 0, 0},
+    {4, 5, 83, 0, 0},
+    {6, 7, 84, 0, 0},
+    {8, 9, 85, 0, 0}
+};
 
 void rot_sw_setup(void)
 {
-    gpio_init(7);
-    gpio_set_dir(7, GPIO_OUT);
+    for (int i = 0; i < MAX_ROTARY_SWITCHES; i++) {
+        gpio_init(rotary_switches[i].clk_pin);
+        gpio_set_dir(rotary_switches[i].clk_pin, GPIO_IN);
+        gpio_pull_up(rotary_switches[i].clk_pin);
 
-    gpio_init(ROT_SW_DATA_PIN);
-    gpio_set_dir(ROT_SW_DATA_PIN, GPIO_IN );
-    gpio_pull_up(ROT_SW_DATA_PIN);
+        gpio_init(rotary_switches[i].data_pin);
+        gpio_set_dir(rotary_switches[i].data_pin, GPIO_IN);
+        gpio_pull_up(rotary_switches[i].data_pin);
 
-    gpio_init(ROT_SW_CLK_PIN);
-    gpio_set_dir(ROT_SW_CLK_PIN, GPIO_IN );
-    gpio_pull_up(ROT_SW_CLK_PIN);
-
-    gpio_set_irq_enabled_with_callback(ROT_SW_CLK_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &sw_interupt_callback);
-    gpio_set_irq_enabled_with_callback(ROT_SW_DATA_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &sw_interupt_callback);
+        gpio_set_irq_enabled_with_callback(rotary_switches[i].clk_pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &sw_interupt_callback);
+        gpio_set_irq_enabled_with_callback(rotary_switches[i].data_pin, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &sw_interupt_callback);
+    }
 }
 
-void update_counter(bool up)
+void update_counter(rotary_switch_t *rot_sw, bool up)
 {
-    if (up)
-    {
-        if (sw_ctr < 127)
-        {
-            sw_ctr += 1;
+    if (up) {
+        if (rot_sw->counter < 127) {
+            rot_sw->counter += 1;
         }
-    }
-    else
-    {
-        if (sw_ctr > 0)
-        {
-            sw_ctr -= 1;
+    } else {
+        if (rot_sw->counter > 0) {
+            rot_sw->counter -= 1;
         }
     }
 }
 
 void sw_interupt_callback(uint gpio, uint32_t events)
 {
-    bool clk = gpio_get(ROT_SW_CLK_PIN);
-    bool data = gpio_get(ROT_SW_DATA_PIN);
+    for (int i = 0; i < MAX_ROTARY_SWITCHES; i++) {
+        rotary_switch_t *rot_sw = &rotary_switches[i];
+        if (gpio == rot_sw->clk_pin || gpio == rot_sw->data_pin) {
+            bool clk = gpio_get(rot_sw->clk_pin);
+            bool data = gpio_get(rot_sw->data_pin);
 
-    if (events & GPIO_IRQ_EDGE_RISE)
-    {
-        if (gpio == ROT_SW_CLK_PIN)
-        {
-            update_counter(data == 0);
-        }
-        else if (gpio == ROT_SW_DATA_PIN)
-        {
-            update_counter(clk == 1);
-        }
-    }
-    else if (events & GPIO_IRQ_EDGE_FALL)
-    {
-        if (gpio == ROT_SW_CLK_PIN)
-        {
-            update_counter(data == 1);
-        }
-        else if (gpio == ROT_SW_DATA_PIN)
-        {
-            update_counter(clk == 0);
+            if (events & GPIO_IRQ_EDGE_RISE) {
+                if (gpio == rot_sw->clk_pin) {
+                    update_counter(rot_sw, data == 0);
+                } else if (gpio == rot_sw->data_pin) {
+                    update_counter(rot_sw, clk == 1);
+                }
+            } else if (events & GPIO_IRQ_EDGE_FALL) {
+                if (gpio == rot_sw->clk_pin) {
+                    update_counter(rot_sw, data == 1);
+                } else if (gpio == rot_sw->data_pin) {
+                    update_counter(rot_sw, clk == 0);
+                }
+            }
         }
     }
 }
 
 void rot_sw_task(void)
 {
-    if ( sw_ctr_last_sent != sw_ctr )
-    {
-        sw_ctr_last_sent = sw_ctr;
-        uint8_t note_on[3] = { 0b10110000 | 0, rot_sw_note, 0x7F & sw_ctr };
-        tud_midi_stream_write( 0, note_on, 3);
+    for (int i = 0; i < MAX_ROTARY_SWITCHES; i++) {
+        rotary_switch_t *rot_sw = &rotary_switches[i];
+        if (rot_sw->last_sent != rot_sw->counter) {
+            rot_sw->last_sent = rot_sw->counter;
+            uint8_t note_on[3] = {0b10110000 | 0, rot_sw->note, 0x7F & rot_sw->counter};
+            tud_midi_stream_write(0, note_on, 3);
+        }
     }
 }
 
--- src/Inputs/RotarySW/rotarty_switch.h
+++ src/Inputs/RotarySW/rotarty_switch.h
@@ -1,30 +1,27 @@
 #ifndef _ROT_SW_H
 #define _ROT_SW_H
 
-
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
-
 #include "hardware/gpio.h"
 #include "tusb.h"
 
+#define MAX_ROTARY_SWITCHES 4
 
+typedef struct 
+{
+    uint8_t clk_pin;
+    uint8_t data_pin;
+    uint8_t note;
+    volatile uint8_t counter;
+    volatile uint8_t last_sent;
+} rotary_switch_t;
 
-#define ROT_SW_CLK_PIN 2
-#define ROT_SW_DATA_PIN 3
-
-extern const uint8_t rot_sw_note; 
-
-extern volatile uint8_t sw_ctr; 
-extern volatile uint8_t sw_ctr_last_sent; 
+extern rotary_switch_t rotary_switches[MAX_ROTARY_SWITCHES];
 
 void rot_sw_setup(void);
-
-
-void sw_interupt_callback();
-
-
+void sw_interupt_callback(uint gpio, uint32_t events);
 void rot_sw_task(void);
 
-#endif 
+#endif
