2433f6ffa74df69d20cc27034bd1b29227136923
PM dev <PMheils.dev@gmail.com>
2025-03-02 10:22:12 +0100

    led setup functions

---
+++ src/Outputs/LED/Digital/led_setup.h
@@ -0,0 +1,13 @@
+#ifndef _LED_HANDLER_H
+#define _LED_HANDLER_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+#include "adc_handler.h"
+
+#define BUTTON_PIN 20
+
+void led_setup(void);
+
+#endif
