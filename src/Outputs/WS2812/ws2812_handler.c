870e69e113fc447846287456717d1556bc914cd9
PM dev <PMheils.dev@gmail.com>
2025-03-05 19:47:11 +0100

    added usefull ws2812 handler functions and structs

--- src/Outputs/WS2812/ws2812_handler.c
+++ src/Outputs/WS2812/ws2812_handler.c
@@ -9,27 +9,62 @@ PIO ws2812_pio;
 uint sm;
 uint offset;
 
+ws2812_pixel_t ws2812_pixels[WS2812_AMT];
+
+uint64_t ws2812_last_update = 0;
 
 void ws2812_init()
 {
-    stdio_init_all();
-    
     pio_claim_free_sm_and_add_program( &ws2812_program, &ws2812_pio, &sm, &offset );
 
     ws2812_program_init( ws2812_pio, sm, offset, WS2812_PIN, 800000, false );
 
-    for ( uint i = 0; i < 100000 ; i++ )
+    ws2812_pixels[0].r = 5;
+    ws2812_pixels[0].g = 0;
+    ws2812_pixels[0].b = 0;
+
+    ws2812_pixels[1].r = 0;
+    ws2812_pixels[1].g = 5;
+    ws2812_pixels[1].b = 0;
+
+    ws2812_pixels[2].r = 0;
+    ws2812_pixels[2].g = 0;
+    ws2812_pixels[2].b = 5;
+
+    ws2812_pixels[3].r = 5;
+    ws2812_pixels[3].g = 5;
+    ws2812_pixels[3].b = 0;
+
+    ws2812_pixels[4].r = 0;
+    ws2812_pixels[4].g = 5;
+    ws2812_pixels[4].b = 5;
+
+    for ( uint i = 0; i < 100; i++ )
     {
-        sleep_us(WS2812_T_REST);
-        ws2812_set_pixel( 1, 1, 1 );
+        ws2812_update();
     }
-    sleep_ms(10);
-    ws2812_set_pixel( 0, 0, 0 );
+
 }
 
-void ws2812_set_pixel( uint8_t r, uint8_t g, uint8_t b )
+static void ws2812_set_pixel( uint8_t r, uint8_t g, uint8_t b )
 {
     uint32_t rgb = ( g << 3*8 ) | ( r << 2*8 ) | ( b << 1*8 );
     pio_sm_put_blocking( ws2812_pio, sm, rgb );
+}
+
+
+void ws2812_update()
+{
+    int64_t time_since_last_update = time_us_64() - ws2812_last_update;
+    if ( time_since_last_update < WS2812_T_REST )
+    {
+        sleep_us( WS2812_T_REST - time_since_last_update );
+    }
+
+    for ( uint i = 0; i < WS2812_AMT; i++ )
+    {
+        ws2812_set_pixel( ws2812_pixels[i].r, ws2812_pixels[i].g, ws2812_pixels[i].b );
+    }
+    ws2812_last_update = time_us_64();
 }
 
--- src/Outputs/WS2812/ws2812_handler.h
+++ src/Outputs/WS2812/ws2812_handler.h
@@ -5,15 +5,27 @@
 
 #define WS2812_PIN 7 
 
+#define WS2812_AMT 5 
+
 // never got an error when using 110 us 
 // some datasheets say 50 us (100 us gives errors), some say 280 us
 #define WS2812_T_REST 500 
 
 #define WS2812_PIO pio0 
 
+typedef struct
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} 
+ws2812_pixel_t;
+
+
 void ws2812_init(); 
 
-void ws2812_set_pixel( uint8_t r, uint8_t g, uint8_t b );
+//void ws2812_set_pixel( uint8_t r, uint8_t g, uint8_t b );
 
+void ws2812_update();
 
 #endif
