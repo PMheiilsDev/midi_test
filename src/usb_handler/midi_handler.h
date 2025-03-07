397abb71ab26985a7c9cd5e5fccb33c50b711459
PM dev <PMheils.dev@gmail.com>
2025-03-07 20:09:55 +0100

    todo added note

--- src/usb_handler/midi_handler.h
+++ src/usb_handler/midi_handler.h
@@ -15,3 +15,10 @@ void process_midi_message(void);
 
 
 #endif
+
+
+
+// notes:
+// to make it possible for the midi_handler to change the values of the inputs 
+// create a struct that holds the pointer to the values of the intputs 
+
