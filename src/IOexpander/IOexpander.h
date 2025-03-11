9a93e6f60a2c266c2c5e43057fd3cdad597e665b
PM dev <PMheils.dev@gmail.com>
2025-03-11 06:11:42 +0100

    added functions declarations with comments

--- src/IOexpander/IOexpander.h
+++ src/IOexpander/IOexpander.h
@@ -19,9 +19,68 @@
 // if a pin is 1 it can be used as input 
 // meaning that the read bit of that pin will be 0 if connected to GND and else 1 (as it has a pull up resistor)
 
+//? ( dont make these extern no access to them from out side )
+//? extern uint16_t IOexpander_pin_func;
+//? extern uint16_t IOexpander_pin_func_pref;
+//? 
+//? extern uint16_t IOexpander_pin_state;
+//? extern uint16_t IOexpander_pin_state_pref;
 
-bool IOexpander_init(void);
+typedef enum 
+{
+    IO_EXPANDER_PIN_FUNC_OUTPUT = 0,
+    IO_EXPANDER_PIN_FUNC_INPUT = 1,
+}
+IOexpander_pin_func_t;
 
+typedef enum 
+{
+    IO_EXPANDER_PIN_OUTPUT_STATE_LOW = 0,
+    IO_EXPANDER_PIN_OUTPUT_STATE_HIGH_Z = 1,
+}
+IOexpander_pin_output_state_t;
+
+typedef enum 
+{
+    IO_EXPANDER_PIN_INPUT_STATE_ERROR = -1,
+    IO_EXPANDER_PIN_INPUT_STATE_LOW = 0,
+    IO_EXPANDER_PIN_INPUT_STATE_HIGH = 1,
+}
+IOexpander_pin_input_state_t;
+
+/// @brief initializes the IOexpander 
+/// @return true if the IOexpander is connected and false if not 
+bool IOexpander_init();
+
+/// @brief sets the function of a ioexpander pin 
+/// @param pin_num number of the pin 0 to 15
+/// @param value from enum IOexpander_pin_func 
+void IOexpander_set_function( uint8_t pin_num, IOexpander_pin_func_t value );
+
+/// @brief get the function of a pin 
+/// @param pin_num number of the pin 0 to 15 
+/// @return 
+IOexpander_pin_func_t IOexpander_get_function( uint8_t pin_num );
+
+/// @brief puts a value on a ioexpander pin (after calling IOexpander_update())
+/// @param pin_num number of the pin 0 to 15 
+/// @param value from enum IOexpander_pin_state 
+void IOexpander_put( uint8_t pin_num, IOexpander_pin_output_state_t value );
+
+/// @brief gets the value of a IO expander input pin
+/// @param pin_num number of the pin 0 to 15 
+/// @param force_update true to update the IO expander via I2C before reading in this function 
+/// @return IOexpander_pin_input_state_t (error if pin is Output pin)
+IOexpander_pin_input_state_t IOexpander_get( uint8_t pin_num, bool force_update );
+
+/// @brief updates the IO expander via I2C (only if changes are done)
+/// @param force force to update even if no changes where done (true recomended at first write)
+/// @return true if changes where done and false if values where already correct 
+bool IOexpander_write( bool force );
+
+/// @brief updates the input pin values of the IO expander via I2C  
+/// @return true if anything changed 
+bool IOexander_read();
 
 #endif
 
