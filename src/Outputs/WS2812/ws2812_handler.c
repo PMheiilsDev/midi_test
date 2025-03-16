5b90b778631c1383513bb9e604c2e7c570ea0b9c
PM dev <PMheils.dev@gmail.com>
2025-03-16 10:04:06 +0100

    added array of the ws2812 elements

--- src/Outputs/WS2812/ws2812_handler.c
+++ src/Outputs/WS2812/ws2812_handler.c
@@ -3,3 +3,43 @@
 
 
 
+WS_2812_t WS_2812[WS2812_AMT] = 
+{
+    // Rotary Switches 
+    {
+        .limit = 127,
+        .value = &(rotary_switches[0].counter),
+    },
+    {
+        .limit = 127,
+        .value = &(rotary_switches[1].counter),
+    },
+    {
+        .limit = 127,
+        .value = &(rotary_switches[2].counter),
+    },
+    {
+        .limit = 127,
+        .value = &(rotary_switches[3].counter),
+    },
+
+    // ADCs
+    {
+        .limit = 127,
+        .value = &(adc_channels[0].result),
+    },
+    {
+        .limit = 127,
+        .value = &(adc_channels[1].result),
+    },
+    {
+        .limit = 127,
+        .value = &(adc_channels[2].result),
+    },
+    {
+        .limit = 127,
+        .value = &(adc_channels[3].result),
+    },
+};
+
+
--- src/Outputs/WS2812/ws2812_handler.h
+++ src/Outputs/WS2812/ws2812_handler.h
@@ -1,5 +1,18 @@
 #ifndef _WS2812_HANDLER_H
 #define _WS2812_HANDLER_H
 
+#include "ws2812.h"
+
+#include "adc_handler.h"
+#include "rotarty_switch.h"
+
+typedef struct 
+{
+    const uint8_t limit;
+    volatile uint8_t* value;
+}
+WS_2812_t;
+
+extern WS_2812_t WS_2812[WS2812_AMT];
 
 #endif
