43d3871ebabb80a9d68b2152cf246be6df80c06c
Phillip <phillip.meisinger@gmx.de>
2025-03-31 11:42:06 +0200

    added comments and prototype

--- src/usb_handler/midi_handler.h
+++ src/usb_handler/midi_handler.h
@@ -7,6 +7,34 @@
 extern char* buffer;
 extern uint64_t buffer_counter;
 
+typedef struct
+{
+    // 4 bytes long 
+    // AAAA BBBB 0CCC CCCC 0DDD DDDD 
+    // A => type of data ( eg control change note on/off ) 
+    // B => Channel number 
+        // C => which note or control it is 
+        // D => velocity or value of control 
+
+    // as voice meeter can use control change 
+    // and note on/off for the sliders
+    // there can be a maximum of 2 * (1<<4) * (1<<7) = 4096 
+
+
+}
+midi_measage_t
+
+typedef struct
+{
+    char changeable : 1;
+    char cable_num : 4;
+
+    uint8_t value;
+    uint8_t measage_type;
+    uint8_t note;
+
+}
+midi_handler_t;
 
 
 void process_midi_message(void);
