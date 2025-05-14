ed143f24d12addef2fe0a146478a8a38aa77ad64
PM dev <PMheils.dev@gmail.com>
2025-05-14 21:42:48 +0200

    all the ioexpander buttons and pins work with each other and are sent via midi

--- src/IOexpander/IOexpander_handler.c
+++ src/IOexpander/IOexpander_handler.c
@@ -5,7 +5,7 @@ IO_exp_midi_element_t IO_exp_midi_element[IO_EXPANDER_MIDI_ELEMENT_AMT] =
 {
     {
         .input_pin = 1,
-        .output_pin = 1,
+        .output_pin = 0,
         .note = 10,
         .state = 0,
         .state_pref = 0
@@ -58,62 +58,6 @@ IO_exp_midi_element_t IO_exp_midi_element[IO_EXPANDER_MIDI_ELEMENT_AMT] =
         .note = 17,
         .state = 0,
         .state_pref = 0
-    },
-    {
-        .input_pin = 8,
-        .output_pin = 5,
-        .note = 18,
-        .state = 0,
-        .state_pref = 0
-    },
-    {
-        .input_pin = 9,
-        .output_pin = 7,
-        .note = 19,
-        .state = 0,
-        .state_pref = 0
-    },
-    {
-        .input_pin = 10,
-        .output_pin = 5,
-        .note = 20,
-        .state = 0,
-        .state_pref = 0
-    },
-    {
-        .input_pin = 11,
-        .output_pin = 6,
-        .note = 21,
-        .state = 0,
-        .state_pref = 0
-    },
-    {
-        .input_pin = 12,
-        .output_pin = 7,
-        .note = 22,
-        .state = 0,
-        .state_pref = 0
-    },
-    {
-        .input_pin = 13,
-        .output_pin = 5,
-        .note = 23,
-        .state = 0,
-        .state_pref = 0
-    },
-    {
-        .input_pin = 14,
-        .output_pin = 6,
-        .note = 24,
-        .state = 0,
-        .state_pref = 0
-    },
-    {
-        .input_pin = 15,
-        .output_pin = 7,
-        .note = 25,
-        .state = 0,
-        .state_pref = 0
     }
 };
 
@@ -124,11 +68,10 @@ void IO_expander_handler_init()
 
     for( uint i = 0; i < IO_EXPANDER_MIDI_ELEMENT_AMT; i++ )
     {
-        //IOexpander_set_function( IO_exp_midi_element[i].output_pin, IO_EXPANDER_PIN_FUNC_OUTPUT );
-        //IOexpander_put( IO_exp_midi_element[i].output_pin, IO_EXPANDER_PIN_OUTPUT_STATE_HIGH_Z );
+        IOexpander_set_function( IO_exp_midi_element[i].output_pin, IO_EXPANDER_PIN_FUNC_OUTPUT );
+        IOexpander_put( IO_exp_midi_element[i].output_pin, IO_EXPANDER_PIN_OUTPUT_STATE_HIGH_Z );
 
-        IOexpander_set_function( IO_exp_midi_element[i].input_pin, IO_EXPANDER_PIN_FUNC_OUTPUT );
-        IOexpander_put( IO_exp_midi_element[i].input_pin, IO_EXPANDER_PIN_OUTPUT_STATE_HIGH_Z );
+        IOexpander_set_function( IO_exp_midi_element[i].input_pin, IO_EXPANDER_PIN_FUNC_INPUT );
     }
 }
 
@@ -142,12 +85,13 @@ void IO_expander_handler_handler()
         if( IO_exp_midi_element[i].state != IO_exp_midi_element[i].state_pref ) 
         {
             IO_exp_midi_element[i].state_pref = IO_exp_midi_element[i].state; 
+            
             uint8_t note_on[3] = {0b10110000 | 0, IO_exp_midi_element[i].note, IO_exp_midi_element[i].state*127};
             tud_midi_stream_write(0, note_on, 3);
 
             if ( IO_exp_midi_element[i].state )
             {
-                //IOexpander_put(IO_exp_midi_element[i].output_pin, !IOexpander_get(IO_exp_midi_element[i].output_pin, false) );
+                IOexpander_put(IO_exp_midi_element[i].output_pin, !IOexpander_get(IO_exp_midi_element[i].output_pin, false) );
             }
         }
     }
